806147b4b0ffac0ccb0556b2b8ee77de37736429
Amal Ziad <131298935+Amalziad1@users.noreply.github.com>
2023-08-20 11:36:56 +0300

    Updated part2.sh

--- part2.sh
+++ part2.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
-#part 2
 
-#variables from environment named run_part2.sh
+printError() {
+    echo "Error: variables from environment are not set"
+    exit 1
+}
+# Variables from environment
 first_name="$FIRST_NAME"
 last_name="$LAST_NAME"
 tmp_dir="$HOME/amal/part2_exec"
 
-#create directories and files
+# Create directories and files
 mkdir -p "$tmp_dir"
 for i in {1..5}; do
     subdir="$tmp_dir/training_project_$i"
     mkdir -p "$subdir"
-    
+    if [ $first_name="" ]
+    then
+   	printError
+    elif [ $last_name="" ]
+    then 
+    	printError
+    fi
     for j in a b; do
         file="$subdir/module_${i}_${j}.txt"
         echo "Hello $first_name $last_name welcome to file ${i}.${j^}" > "$file"
